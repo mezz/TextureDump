ace5d5d98ff5dd4c323a7343ffc22cf07bfbaf94
mezz <tehgeek@gmail.com>
2016-10-24 14:58:52 -0700

    Version 1.1

--- src/main/java/mezz/texturedump/TextureDump.java
+++ src/main/java/mezz/texturedump/TextureDump.java
@@ -29,7 +29,7 @@ import org.lwjgl.opengl.GL12;
 @Mod(modid = TextureDump.MODID, name = "Texture Dump", version = TextureDump.VERSION, clientSideOnly = true)
 public class TextureDump {
 	public static final String MODID = "texturedump";
-	public static final String VERSION = "1.0";
+	public static final String VERSION = "1.1";
 
 	@Mod.EventHandler
 	public void preInit(FMLPreInitializationEvent event) {
