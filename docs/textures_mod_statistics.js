493f170fa958f48e895927b6c5ab9ffbd4afb2ed
Steven Blom <atomicblom@binaryvibrance.net>
2016-11-02 15:36:13 +1030

    Update Demo

---
+++ docs/textures_mod_statistics.js
@@ -0,0 +1,12 @@
+var modStatistics = 
+//Start of Data
+[
+    {
+        "resourceDomain": "minecraft",
+        "pixelCount": 193792,
+        "percentOfTextureMap": 100.0,
+        "modName": "Minecraft",
+        "url": "",
+        "authors": []
+    }
+]
