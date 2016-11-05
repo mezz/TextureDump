bb343721175dbddfc6b70d0685c3b3d71cf8812b
mezz <tehgeek@gmail.com>
2016-11-04 20:14:44 -0700

    Update example site

--- docs/textures_mod_statistics.js
+++ docs/textures_mod_statistics.js
@@ -4,15 +4,15 @@ var modStatistics =
     {
         "resourceDomain": "minecraft",
         "pixelCount": 193792,
-        "percentOfTextureMap": 41.186073303222656,
+        "percentOfTextureMap": 42.720088958740234,
         "modName": "Minecraft",
         "url": "",
         "authors": []
     },
     {
         "resourceDomain": "forestry",
-        "pixelCount": 187136,
-        "percentOfTextureMap": 39.77149200439453,
+        "pixelCount": 160256,
+        "percentOfTextureMap": 35.32731246948242,
         "modName": "Forestry",
         "url": "http://forestry.sengir.net/",
         "authors": [
@@ -23,7 +23,7 @@ var modStatistics =
     {
         "resourceDomain": "rftools",
         "pixelCount": 77568,
-        "percentOfTextureMap": 16.485309600830078,
+        "percentOfTextureMap": 17.099323272705078,
         "modName": "RFTools",
         "url": "",
         "authors": [
@@ -33,15 +33,35 @@ var modStatistics =
     {
         "resourceDomain": "rftoolscontrol",
         "pixelCount": 11264,
-        "percentOfTextureMap": 2.393906354904175,
+        "percentOfTextureMap": 2.483069896697998,
         "modName": "RFTools Control",
         "url": "",
         "authors": []
     },
     {
+        "resourceDomain": "chiselsandbits",
+        "pixelCount": 8960,
+        "percentOfTextureMap": 1.97516930103302,
+        "modName": "Chisels & Bits",
+        "url": "http://minecraft.curseforge.com/projects/chisels-bits",
+        "authors": [
+            "AlgorithmX2"
+        ]
+    },
+    {
+        "resourceDomain": "flatcoloredblocks",
+        "pixelCount": 1024,
+        "percentOfTextureMap": 0.22573363780975342,
+        "modName": "Flat Colored Blocks",
+        "url": "http://minecraft.curseforge.com/projects/flat-colored-blocks",
+        "authors": [
+            "AlgorithmX2"
+        ]
+    },
+    {
         "resourceDomain": "forge",
         "pixelCount": 768,
-        "percentOfTextureMap": 0.16322089731693268,
+        "percentOfTextureMap": 0.16930022835731506,
         "modName": "Minecraft Forge",
         "url": "http://minecraftforge.net",
         "authors": [
