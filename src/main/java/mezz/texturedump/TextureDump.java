f14bfa7ce6a74d009a5d071f10ea902966e4535d
mezz <tehgeek@gmail.com>
2016-11-06 01:53:23 -0800

    Detect other texture maps that are registered with the texture manager

--- src/main/java/mezz/texturedump/TextureDump.java
+++ src/main/java/mezz/texturedump/TextureDump.java
@@ -1,13 +1,19 @@
 package mezz.texturedump;
 
 import java.io.File;
+import java.util.Map;
 
 import mezz.texturedump.dumpers.ModStatsDumper;
 import mezz.texturedump.dumpers.TextureImageDumper;
 import mezz.texturedump.dumpers.TextureInfoDumper;
 import mezz.texturedump.util.Log;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiMainMenu;
+import net.minecraft.client.renderer.texture.ITextureObject;
+import net.minecraft.client.renderer.texture.TextureManager;
 import net.minecraft.client.renderer.texture.TextureMap;
-import net.minecraftforge.client.event.TextureStitchEvent;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.client.event.GuiOpenEvent;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
@@ -17,6 +23,7 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 public class TextureDump {
 	public static final String MODID = "texturedump";
 	public static final String VERSION = "@VERSION@";
+	private boolean dumped = false;
 
 	@Mod.EventHandler
 	public void preInit(FMLPreInitializationEvent event) {
@@ -24,12 +31,21 @@ public class TextureDump {
 	}
 
 	@SubscribeEvent
-	public void postTextureStitch(TextureStitchEvent.Post event) {
-		TextureMap map = event.getMap();
-		if (map.mapUploadedSprites.size() <= 1) {
-			return; // skip the first texture stitch event that only contains the missing texture
+	public void onMainMenuOpen(GuiOpenEvent event) {
+		if (!dumped && event.getGui() instanceof GuiMainMenu) {
+			dumped = true;
+			TextureManager textureManager = Minecraft.getMinecraft().getTextureManager();
+			for (Map.Entry<ResourceLocation, ITextureObject> entry : textureManager.mapTextureObjects.entrySet()) {
+				ITextureObject textureObject = entry.getValue();
+				if (textureObject instanceof TextureMap) {
+					String name = entry.getKey().toString().replace(':', '_').replace('/', '_');
+					dumpTextureMap((TextureMap) textureObject, name);
+				}
+			}
 		}
-		String name = map.getBasePath().replace('/', '_');
+	}
+
+	private void dumpTextureMap(TextureMap map, String name) {
 		int mip = map.getMipmapLevels();
 		File outputFolder = new File("texture_dump");
 		if (!outputFolder.exists()) {
